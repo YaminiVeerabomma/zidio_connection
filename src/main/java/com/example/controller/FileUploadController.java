2e9e354257528aebf593a3372c003935f530c89e
YaminiVeerabomma <yaminiveerabomma@gmail.com>
2025-08-05 22:19:13 +0530

    fileupload

--- src/main/java/com/example/controller/FileUploadController.java
+++ src/main/java/com/example/controller/FileUploadController.java
@@ -1,5 +1,6 @@
 package com.example.controller;
 
+import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -9,24 +10,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
 import com.example.service.FileUploadService;
 
-
 @RestController
-@RequestMapping("/api/upload")
+@RequestMapping("/api/uploadFile")
 public class FileUploadController {
 
-	@Autowired 
-	private FileUploadService fileUploadService;
-	
-	@PostMapping(value="/resume",produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<String> uploadResume(@RequestParam("file")MultipartFile file){
-		try {
-			String url =fileUploadService.uploadFile(file);
-			return ResponseEntity.ok(url);
-			
-		} catch (Exception e) {
-			return ResponseEntity.badRequest().body("Upload failed"+ e.getMessage());
-		}
-	}
+    @Autowired 
+    private FileUploadService fileUploadService;
+
+    @PostMapping(value="/resume", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> uploadResume(@RequestParam("file") MultipartFile file) throws IOException {
+        return ResponseEntity.ok(fileUploadService.uploadFile(file, "resume"));
+    }
+
+    @PostMapping(value="/image", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> uploadCertificate(@RequestParam("file") MultipartFile file) throws IOException {
+        return ResponseEntity.ok(fileUploadService.uploadFile(file, "certificate"));
+    }
+
+    @PostMapping(value="/invoice", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> uploadInvoice(@RequestParam("file") MultipartFile file) throws IOException {
+        return ResponseEntity.ok(fileUploadService.uploadFile(file, "invoice"));
+    }
 }
--- src/main/java/com/example/service/FileUploadService.java
+++ src/main/java/com/example/service/FileUploadService.java
@@ -9,6 +9,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
+import com.example.security.CloudinaryConfig;
 
 @Service
 
@@ -16,17 +17,20 @@ public class FileUploadService {
 	
 	@Autowired
 	private Cloudinary cloudinary;
+
+	@Autowired
+	private CloudinaryConfig coludinaryConfig;
 	
-	public String uploadFile(MultipartFile file){
-		
+	public String uploadFile(MultipartFile file, String folder) throws IOException{
 		try {
-			Map<String,Object> uploadResult = cloudinary.uploader().upload(file.getBytes(),ObjectUtils.emptyMap());
-			return uploadResult.get("secure_url").toString();
+			Map uploadFile= coludinaryConfig.cloudinary().uploader().upload(file.getBytes(),ObjectUtils.asMap("folder",folder));
+			
+			return (String)uploadFile.get("secure_url");
 		} catch (IOException e) {
+			
 			e.printStackTrace();
-			throw new RuntimeException("Faild to upload files",e);
 		}
-		
+		 return null;
 	}
 
 }
