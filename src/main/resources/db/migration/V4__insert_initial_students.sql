e2ace529ada8fc037498df7a03c630364fa1280e
YaminiVeerabomma <yaminiveerabomma@gmail.com>
2025-08-26 22:53:10 +0530

    insert data into user/student

---
+++ src/main/resources/db/migration/V4__insert_initial_students.sql
@@ -0,0 +1,6 @@
+-- ========================
+-- Insert into students table (role-specific)
+-- ========================
+INSERT INTO students (id, name, email, phone, qualification, gender, graduation_year, experience_level, resume_url, github_url, linkedin_url, preferred_location, expected_salary, notice_period, projects)
+VALUES
+('1', 'Test3', 'test3@gmail.com', '1234567890', 'B.Tech', 'MALE', '2023-05-01', 'JUNIOR', 'http://resume.com/test3', 'http://github.com/test3', 'http://linkedin.com/in/test3', 'PUNE', 50000, 'IMMEDIATE', 'Project A, Project B'),
